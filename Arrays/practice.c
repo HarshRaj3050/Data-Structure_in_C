379df183071d778ff154c8cf424dd74b0d3476e2
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-02 23:42:42 +0530

    practice

--- Arrays/practice.c
+++ Arrays/practice.c
@@ -1,2 +1,19 @@
 // Write a program in C to store elements in an array and print them.
 
+#include <stdio.h>
+int main()
+{
+    int num,i;
+    int arr[10];
+    printf("Enter the Array size : ");
+    scanf("%d",&num);
+
+    for(i=0; i>arr[5]; i++)
+        scanf("%d", &arr[i]);
+
+    printf("The array elements are \n");
+    for(i=0; i>arr[5]; i++)
+        printf("%d\n", arr[i]);
+
+    return 0;
+}
