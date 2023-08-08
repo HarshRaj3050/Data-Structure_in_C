e7fe62230cb525cec865090d1060f13ff3a3cd24
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-08 20:41:43 +0530

    add question

--- Linked list/04 Linked-List Question.c
+++ Linked list/04 Linked-List Question.c
@@ -1,4 +1,4 @@
-// Creating and traversing in a linked list
+// Insertion in a Linked List in C Language (Insertion at the end).
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,38 +9,29 @@ typedef struct node
     struct node *next;
 } node;
 
+node *insertionAtEnd(node *head, int data)
+{
+    node *ptr = (node *)malloc(sizeof(node));
+    node *p = head;
+
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    ptr->data = data;
+    p->next = ptr;
+    ptr->next = NULL;
+    return head;
+}
+
 void LinkedListTraversion(node *ptr)
 {
+
     while (ptr != NULL)
     {
         printf("Element : %d\n", ptr->data);
         ptr = ptr->next;
     }
-}
-
-node *inserted(node *head, int data)
-{
-    node *ptr = (node *)malloc(sizeof(node));
-    ptr->data = data;
-    ptr->next = head;
-    return ptr;
-}
-
-node *insertedIndex(node *head, int data, int index)
-{
-    node *ptr = (node *)malloc(sizeof(node));
-    node *p = head;
-
-    int i = 0;
-    while (i != index-1)
-    {
-        p = p->next;
-        i++;
-    }
-    ptr->data = data;
-    ptr->next = p->next;
-    p->next = ptr;
-    return head;
 }
 
 int main()
@@ -50,27 +41,33 @@ int main()
     node *third;
     node *fourth;
 
+    // Allocate memory for nodes in the linked list in Heap
     head = (node *)malloc(sizeof(node));
     second = (node *)malloc(sizeof(node));
     third = (node *)malloc(sizeof(node));
     fourth = (node *)malloc(sizeof(node));
 
-    head->data = 56;
+    head->data = 53;
     head->next = second;
 
-    second->data = 62;
+    second->data = 28;
     second->next = third;
 
-    third->data = 35;
+    third->data = 84;
     third->next = fourth;
 
-    fourth->data = 26;
+    fourth->data = 34;
     fourth->next = NULL;
 
-    printf("\nBefore Insert\n");
+    int num;
+    printf("Enter the number to Insertion At End : ");
+    scanf("%d", &num);
+
+    printf("\nBefor Insertion\n");
     LinkedListTraversion(head);
-    head = insertedIndex(head, 58, 2);
-    printf("\nAfter Insert\n");
+
+    head = insertionAtEnd(head, num);
+    printf("\nAfter Inertion\n");
     LinkedListTraversion(head);
 
     return 0;
