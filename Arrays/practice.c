e2d6c161fe4ac3154ad9b3fc42e9df86abb193e3
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-20 23:46:26 +0530

    practice

--- Arrays/practice.c
+++ Arrays/practice.c
@@ -1,49 +1,24 @@
-// C Program To find biggest and smallest number and positions in array
+#include <stdio.h>
 
-#include<stdio.h>
+int seriesSum(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n + seriesSum(n - 1);
+    }
+}
+
 int main()
 {
-    int i,num, numarr, max, min, maxpos, minpos;
-    printf("Enter the array size : ");
-    scanf("%d", &num);
+    int n, sum;
+    printf("Enter the number : ");
+    scanf("%d", &n);
 
-    printf("\nINPUT THE %d ELEMENTS \n", num);
-    int arr[num];
-    numarr = num + 1;
-
-    for(i = 1; i < numarr; i++)
-    {
-        printf("%d. Enter the Number : ", i);
-        scanf("%d", &arr[i]);
-    }
-
-    max = 0;
-    maxpos = 1;
-    for(i = 1; i < numarr; i++)
-    {
-  
-        if (max < arr[i])
-        {
-            max = arr[i];
-            maxpos = i;
-        }
-    }
-
-    min = max;
-    minpos = 1;
-    for(i = 1; i < numarr; i++)
-    {
-  
-        if (min > arr[i])
-        {
-            min = arr[i];
-            minpos = i;
-        }
-    }
-
-    printf("\nThe Biggest element of Array is : %d \n", max);
-    printf("The Biggest element position is : %d \n", maxpos);
-    printf("\nThe Smallest element of Array is : %d \n", min);
-    printf("The Smallest element position is : %d \n", minpos);
+    sum = seriesSum(n);
+    printf("\nThe sum of all the n intger is : %d", sum);
     return 0;
 }
