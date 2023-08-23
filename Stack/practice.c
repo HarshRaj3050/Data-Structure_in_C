cdd214cb6504982047d5488f763cffbac6985f30
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-23 23:51:56 +0530

    pratice

--- Stack/practice.c
+++ Stack/practice.c
@@ -1,1 +1,55 @@
 // Implementing the pop operations.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node 
+{
+    int size;
+    int top;
+    struct node *arr;
+}node;
+
+int isFull(node *ptr)
+{
+    if(ptr->top == ptr->size-1)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isEmpty(node *ptr)
+{
+    if(ptr->top == -1)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+void push(node *ptr, int value)
+{
+    if(isFull(ptr))
+    {
+        printf("The stack is overflow, %d is cannot be push", value);
+    }
+    else
+    {
+        ptr->top++;
+        ptr->arr[ptr->top] = value;
+    }
+}
+
+int main()
+{
+    node *sp = (node *)malloc(sizeof(node));
+    sp->size = 10;
+    sp->top = -1;
+    sp->arr = (int *)malloc(sp->size * sizeof(int));
+
+
+
+
+    return 0;
+}
