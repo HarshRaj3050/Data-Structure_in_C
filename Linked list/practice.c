8a9def9989615f1d0efbb890bf564ae6b26b5f1f
HarshRaj3050 <harshraj522004@gmail.com>
2023-08-31 23:41:23 +0530

    update

--- Linked list/practice.c
+++ Linked list/practice.c
@@ -1,38 +1,15 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<stlib.h>
 
-typedef struct node
+typedef struct *node
 {
     int data;
     struct node *next;
-} node;
-
-void LinkedListTraversal(node *ptr)
-{
-    while(ptr != NULL)
-    {
-        printf("element : %d\n", ptr->data);
-        ptr = ptr->next;
-    }
+    struct node *pre;
 }
 
 int main()
 {
-    node *head = (node *)malloc(sizeof(node));
-    node *p1 = (node *)malloc(sizeof(node));
-    node *p2 = (node *)malloc(sizeof(node));
-    node *p3 = (node *)malloc(sizeof(node));
-    
-    head->data = 53;
-    head->next = p1;
-    p1->data = 34;
-    p1->next = p2;
-    p2->data = 63;
-    p2->next = p3;
-    p3->data = 83;
-    p3->next = NULL;
 
-    LinkedListTraversal(head);
-    
     return 0;
 }
