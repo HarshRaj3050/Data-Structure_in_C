d3c9013d0fe3503a6b66641c7b95537b0bb33d10
HarshRaj3050 <harshraj522004@gmail.com>
2023-09-05 23:40:23 +0530

    update

--- Tree/05 Tree Code.c
+++ Tree/05 Tree Code.c
@@ -17,17 +17,49 @@ node *createNode(int data)
     return ptr;
 }
 
+void preOrder(node *root)
+{
+    if(root != NULL)
+    {
+        printf("%d ", root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+int isBST(node *root)
+{
+    static node *prev = NULL;
+    if(root!=NULL)
+    {
+        if(!isBST(root->left))
+        {
+            return 0;
+        }
+        if(prev != NULL && root->data <= prev->data)
+        {
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
+    }
+    else
+    {
+        return 1;
+    }
+}
+
 int main()
 {
     node *root = createNode(63);
-    node *p1 = createNode(29);
-    node *p2 = createNode(53);
-    node *p3 = createNode(82);
-
-    root->left = p1;
-    root->right = p2;
-    p1->left = p3;
+    root->left = createNode(38);
+    root->right = createNode(98);
+    root->left->left = createNode(18);
     
+    preOrder(root);
+    printf("\n");
+    printf("%d", isBST(root));
+
     
     return 0;
 }
