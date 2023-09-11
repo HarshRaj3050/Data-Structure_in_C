007d66b3c313384298b8675aae004a4e6900aa9f
HarshRaj3050 <harshraj522004@gmail.com>
2023-09-11 23:46:56 +0530

    practice

--- Tree/practice.c
+++ Tree/practice.c
@@ -1,26 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct node
-{
-    int data;
-    struct node *left;
-    struct node *right;
-}node;
-
-node *createNode(int data)
-{
-    node *ptr = (node *)malloc(sizeof(node));
-    ptr->data = data;
-    ptr->left = NULL;
-    ptr->right = NULL;
-    return ptr;
-}
-
 int main()
 {
-    node *root;
-    node *p1 = createNode(53);
-    
+
     return 0;
 }
