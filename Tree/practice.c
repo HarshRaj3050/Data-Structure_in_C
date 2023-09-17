c4c48b9225afa6474930c453835909dc2e2d9aaf
HarshRaj3050 <harshraj522004@gmail.com>
2023-09-17 23:42:50 +0530

    practice

--- Tree/practice.c
+++ Tree/practice.c
@@ -4,23 +4,26 @@
 typedef struct node
 {
     int data;
-    struct node *next;
-    struct node *prev;
+    struct node *left;
+    struct node *right;
 }node;
 
 node *createTree(int data)
 {
     node *ptr = (node *)malloc(sizeof(node));
     ptr->data = data;
-    ptr->next = NULL;
-    ptr->prev = NULL;
+    ptr->left = NULL;
+    ptr->right = NULL;
     return ptr;
 }
 
 int main()
 {
-    node *root = (node *)malloc(sizeof(node));
-
+    node *root = createTree(64);
+    root->left = createTree(34);
+    root->right = createTree(83);
+    root->left->left = createTree(23);
+    root->left->right = createTree(31);
 
     return 0;
 }
