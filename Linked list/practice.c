f3b079f5fd4bc5397e90639a160c6a05de7bff4c
HarshRaj3050 <harshraj522004@gmail.com>
2023-09-27 23:40:41 +0530

    practice

--- Linked list/practice.c
+++ Linked list/practice.c
@@ -9,59 +9,26 @@ typedef struct node
     struct node *next;
 }node;
 
-void LinkedListTraversal(node *head)
-{
-    node *ptr = head;
-    while(ptr != NULL)
-    {
-        printf("Element : %d\n", ptr->data);
-        ptr = ptr->next;
-    }
-}
-
-int array(num)
-{
-    int i=0,a[5];
-    while(i>5)
-    {
-        printf("%d. Enter the number to insert the LinkedList : \n", i+1);
-        scanf("%d", &a[i]);
-        i++;
-    }
-    return a[num];
-}
-
 int main()
 {
+    node *head = (node *)malloc(sizeof(node));
+    node *second = (node *)malloc(sizeof(node));
+    node *third = (node *)malloc(sizeof(node));
+    node *fourth = (node *)malloc(sizeof(node));
 
-    int num[5];
-    num[5] = array(num);
-
-    node *head;
-    node *second;
-    node *third;
-    node *fourth;
-
-    head = (node *)malloc(sizeof(node));
-    second = (node *)malloc(sizeof(node));
-    third = (node *)malloc(sizeof(node));
-    fourth = (node *)malloc(sizeof(node));
-
-
-    head->data = num[0];
+    head->data = 63;
     head->next = second;
 
-    second->data = num[1];
+    second->data = 82;
     second->next = third;
 
-    third->data = num[2];
+    third->data = 28;
     third->next = fourth;
-    
-    fourth->data = num[3];
+
+    fourth->data = 93;
     fourth->next = NULL;
-    
-    printf("----- The all the Linked List Element are ------\n");
-    LinkedListTraversal(head);
+
+
 
     return 0;
 }
